0a4ca92bfa9ff83f80e2cf3d552e6b0d02877f63
fredyfirmansantoso-art <fredyfirmansantoso@gmail.com>
2026-07-20 03:10:14 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+const pushback = new Pushback();
 
+document.addEventListener("keydown", e => {
+
+    if (e.shiftKey && e.code === "KeyP") {
+        pushback.toggle();
+    }
+
+});
+
+setInterval(() => {
+
+    pushback.update();
+
+},20);
