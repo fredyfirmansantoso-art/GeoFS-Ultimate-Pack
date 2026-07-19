c021f181c0a0d41a0311a64d4e57a3c80eb46ccf
fredyfirmansantoso-art <fredyfirmansantoso@gmail.com>
2026-07-20 03:09:50 +0700

    pushback.js

--- src/pushback.js
+++ src/pushback.js
@@ -1,1 +1,58 @@
+class Pushback {
 
+    constructor() {
+        this.enabled = false;
+        this.speed = 0.6;
+        this.direction = "back";
+    }
+
+    toggle() {
+        this.enabled = !this.enabled;
+
+        console.log(
+            "[UltimatePack] Pushback",
+            this.enabled ? "Enabled" : "Disabled"
+        );
+    }
+
+    update() {
+
+        if (!this.enabled) return;
+
+        const aircraft = geofs.aircraft.instance;
+
+        if (!aircraft) return;
+
+        switch (this.direction) {
+
+            case "back":
+                aircraft.llaLocation[0] -= this.speed * 0.0000002;
+                break;
+
+            case "forward":
+                aircraft.llaLocation[0] += this.speed * 0.0000002;
+                break;
+
+        }
+
+    }
+
+    left() {
+        this.direction = "left";
+    }
+
+    right() {
+        this.direction = "right";
+    }
+
+    forward() {
+        this.direction = "forward";
+    }
+
+    backward() {
+        this.direction = "back";
+    }
+
+}
+
+window.Pushback = Pushback;
